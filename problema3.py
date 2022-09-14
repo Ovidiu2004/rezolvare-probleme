09429a666695c919b536eecec61f06788988c0f4
Frecautanu Ovidiu <70950196+Ovidiu2004@users.noreply.github.com>
2022-09-14 11:07:50 +0300

    Add files via upload

---
+++ problema3.py
@@ -0,0 +1,9 @@
+a = input("introduceti numarul ")
+b = input("introduceti numarul ")
+c = input("introduceti numarul ")
+d = input("introduceti numarul ")
+def adunareainmultireafractiei():
+    print(f"suma e{((a*d) + (c*b)) / (b*d)},iar produsul {(a*c)) / (b*d)}')
+
+adunareainmultireafractiei()
+  
